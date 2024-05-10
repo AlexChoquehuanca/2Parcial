841ac4764d3d7dad3c54de71894bc8969cc08ee9
ALEX ADRIAN <alexadrianchy@LAPTOP-K95VR1IJ.localdomain>
2024-05-10 09:03:23 -0500

    segundo_componente

---
+++ REACT-PROYECT-EXAMEN/src/componentes/componente2.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./componente1.css"
+
+function componente1() {
+
+  return (
+      <div className='ContenedorDinamico'>
+            <img src="https://s3-alpha-sig.figma.com/img/32f8/b376/8bed0ee79117e6d26de911a4ac519346?Expires=1716163200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=iP4ymKULlxyjLZ9vuRE2eoB9-5lnMehgecQ-EqgdtZW6cOO8PDxYA4rEQsS73qV-gzP5C8NFw3f~206cJPEvt-mEX68LyDOmBR6cUo08Wp4FVhfrIGuZbhgS~TdGyKXs0T6-cOb3gyojXjUv9keW8RYivf1Xy16KyvcdnCK1dfGWEgFSe~srnKMYd-nHi~2KnfCB9bFp6PrwE1SBO1lULw6x06qcnpT02s5N-QXjqXlol8i2aocwwXyjoQFIG24fox5PzAGKT2x4nkxqzun7xQEP4uxDzDwfY6s5YjEaVGebUYy4-Cs2vRURs4M2qVoNsLeAK0uCJq-eOy3TAKGNSA__" alt="10px" />
+      </div>
+        
+  )
+}
+
+export default componente1
